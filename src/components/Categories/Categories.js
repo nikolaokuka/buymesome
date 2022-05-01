9b3e3cf1d66554c01fffad0f4c6d58b4a36b5687
Nikola Okuka <nikolaokuka@gmail.com>
2022-05-01 10:52:18 +0200

    add Categories and Category components

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,14 @@
+import './Categories.scss'
+import Category from '../Category/Category'
+
+const Categories = ({ categories }) => {
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <Category key={category.id} category={category} />
+      ))}
+    </div>
+  )
+}
+
+export default Categories
