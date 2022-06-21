3d9154abd4d2c99ee3322406f179ba372edc714d
Nikola Okuka <nikolaokuka@gmail.com>
2022-06-21 13:40:34 +0200

    typing user saga

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,10 +1,10 @@
-import { all, call } from 'redux-saga/effects';
+import { all, call } from 'typed-redux-saga/macro';
 
 import { categoriesSaga } from './categories/categories.sagas';
 import { userSaga } from './user/user.sagas';
 
 export function* rootSaga() {
-  yield all([
+  yield* all([
     call(categoriesSaga),
     call(userSaga),
   ]);
---
+++ src/store/user/user.sagas.ts
@@ -0,0 +1,129 @@
+import { all, call, put, takeLatest } from 'typed-redux-saga/macro';
+
+import { USER_ACTION_TYPES } from './user.types';
+import { User } from 'firebase/auth';
+
+import {
+  signInSuccess,
+  signInFailed,
+  signUpSuccess,
+  signUpFailed,
+  signOutSuccess,
+  signOutFailed,
+  EmailSignInStart,
+  SignUpStart,
+  SignUpSuccess,
+} from './user.actions';
+
+import {
+  getCurrentUser,
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInAuthUserWithEmailAndPassword,
+  createAuthUserWithEmailAndPassword,
+  signOutUser,
+  AdditionalInformation,
+} from '../../utils/firebase/firebase';
+
+function* getSnapshotFromUserauth(userAuth: User, additionalDetails?: AdditionalInformation) {
+  try {
+    const userSnapshot = yield* call(createUserDocumentFromAuth, userAuth, additionalDetails);
+    if (userSnapshot) {
+      yield* put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    }
+  } catch (error) {
+    console.log(error);
+    yield* put(signInFailed(error as Error));
+  }
+}
+
+function* signUp({ payload: { email, password, displayName } }: SignUpStart) {
+  try {
+    const userCredential = yield* call(createAuthUserWithEmailAndPassword, email, password);
+    if (userCredential) {
+      const { user } = userCredential;
+      yield* put(signUpSuccess(user, { displayName }));
+    }
+  } catch (error) {
+    yield* put(signUpFailed(error as Error));
+  }
+}
+
+function* signInAfterSignUp({ payload: { user, additionalDetails } }: SignUpSuccess) {
+  yield* call(getSnapshotFromUserauth, user, additionalDetails);
+}
+
+function* signInWithGoogle() {
+  try {
+    const { user } = yield* call(signInWithGooglePopup);
+    yield* call(getSnapshotFromUserauth, user);
+  } catch (error) {
+    yield* put(signInFailed(error as Error));
+  }
+}
+
+function* signInWithEmail({ payload: { email, password } }: EmailSignInStart) {
+  try {
+    const userCredential = yield* call(signInAuthUserWithEmailAndPassword, email, password);
+    if (userCredential) {
+      const { user } = userCredential;
+      yield* call(getSnapshotFromUserauth, user);
+    }
+  } catch (error) {
+    yield* put(signInFailed(error as Error));
+  }
+}
+
+function* signOut() {
+  try {
+    yield* call(signOutUser);
+    yield* put(signOutSuccess());
+  } catch (error) {
+    yield* put(signOutFailed(error as Error));
+  }
+}
+
+function* isUserAuthenticated() {
+  try {
+    const userAuth = yield* call(getCurrentUser);
+    if (!userAuth) return;
+    yield* call(getSnapshotFromUserauth, userAuth);
+  } catch (error) {
+    yield* put(signInFailed(error as Error));
+  }
+}
+
+function* onSignUpStart() {
+  yield* takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+}
+
+function* onSignUpSuccess() {
+  yield* takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
+function* onGoogleSignInStart() {
+  yield* takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
+
+function* onEmailSignInStart() {
+  yield* takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
+function* onCheckUserSession() {
+  yield* takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+function* onSignOutStart() {
+  yield* takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
+}
+
+export function* userSaga() {
+  yield* all([
+    call(onCheckUserSession),
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+    call(onSignUpStart),
+    call(onSignUpSuccess),
+    call(onSignOutStart),
+  ]);
+}
