f25f671ad19468fdf8483e275fb9c4cf1c426825
Nikola Okuka <nikolaokuka@gmail.com>
2022-06-21 13:04:06 +0200

    typing categories sagas

--- src/store/categories/categories.sagas.ts
+++ src/store/categories/categories.sagas.ts
@@ -1,4 +1,4 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'typed-redux-saga';
 
 import { CATEGORIES_ACTION_TYPES } from './categories.types';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase';
@@ -9,19 +9,19 @@ import {
 
 function* fetchCategoriesAsync() {
   try {
-    const categories = yield call(getCategoriesAndDocuments);
-    yield put(fetchCategoriesSuccess(categories));
+    const categories = yield* call(getCategoriesAndDocuments);
+    yield* put(fetchCategoriesSuccess(categories));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error.message));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 };
 
 function* onFetchCategoriesAsync() {
-  yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
+  yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync);
 }
 
 export function* categoriesSaga() {
-  yield all([
+  yield* all([
     call(onFetchCategoriesAsync)
   ]);
 }
