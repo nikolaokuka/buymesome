3158c32bd0ee27090851fb18b11ee72218c3f8d2
Nikola Okuka <nikolaokuka@gmail.com>
2022-06-27 18:37:26 +0200

    add dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/Home/Home.component';
-import Navigation from './routes/Navigation/Navigation.component';
-import Authentication from './routes/Authentication/Authentication.component';
-import Shop from './routes/Shop/Shop.component';
-import Checkout from './routes/Checkout/Checkout.component';
 import { checkUserSession } from './store/user/user.actions';
+import Spinner from './components/Spinner/Spinner.component';
+
+const Home = lazy(() => import('./routes/Home/Home.component'));
+const Navigation = lazy(() => import('./routes/Navigation/Navigation.component'));
+const Authentication = lazy(() => import('./routes/Authentication/Authentication.component'));
+const Shop = lazy(() => import('./routes/Shop/Shop.component'));
+const Checkout = lazy(() => import('./routes/Checkout/Checkout.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,14 +19,16 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
